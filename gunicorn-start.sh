6ce3e3caed31705be7c9c82b085c437d5f94b1a2
Vincent Danen <vdanen@redhat.com>
2025-01-26 15:22:57 -0700

    bind to localhost rather than all

--- gunicorn-start.sh
+++ gunicorn-start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 source .venv/bin/activate
-.venv/bin/gunicorn -w 4 -b 0.0.0.0:5000 --access-logfile - --timeout 90 -k gevent 'app:create_app()'
+.venv/bin/gunicorn -w 4 -b 127.0.0.1:5000 --access-logfile - --timeout 90 -k gevent 'app:create_app()'
 deactivate
